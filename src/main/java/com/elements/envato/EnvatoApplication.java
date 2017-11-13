5d76a903549eb4e163cb560b4338e8aec33dcf4c
licod1997 <licod1997@gmail.com>
2017-11-14 00:04:45 +0900

    Initial commit

---
+++ src/main/java/com/elements/envato/EnvatoApplication.java
@@ -0,0 +1,12 @@
+package com.elements.envato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvatoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/elements/envato/EnvatoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.elements.envato;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnvatoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
